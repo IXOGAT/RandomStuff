d726d93256f52d20fc40ab01b4f03714de3ccab7
Alex Mononen <almo121@live.com>
2017-10-03 18:29:46 -0700

    Nearing completion. still need to do internal derivative calculation

--- NewtonsMethod/NewtonsMethod/NewtonsMethod.c
+++ NewtonsMethod/NewtonsMethod/NewtonsMethod.c
@@ -16,57 +16,71 @@ int main(void)
 	const int seedValue = 1;
 	char function[128] = "\0";
 	char workingFunction[128] = "\0";
-	char targetVar = 'x';
-	//printf("What is the function that you want to find the zeros of?\n> ");
-	//scanf_s("%127[^\n]", function);
 	int rc = getLine("What is the function that you want to find the zeros of?\n> ", function, sizeof(function));
 	if (rc == NO_INPUT) {
 		// Extra NL since my system doesn't output that on EOF.
 		printf("\nNo input\n");
 		return 1;
 	}
-
 	if (rc == TOO_LONG) {
 		printf("Input too long [%s]\n", function);
 		return 1;
 	}
 	char deriv[128] = "\0";
-	int targetVarPos[20];
-	//find all x's in input string.
-	int n = 0;
-	for (int i = 0; i < strlen(function); i++)
+	int cr = getLine("what is the derivative of the function (auto deriv calc will be added later)?\n", deriv, sizeof(deriv));
+	if (cr == NO_INPUT)
 	{
-		if (function[i] == targetVar)
-		{
-			targetVarPos[n] = i;
-			n++;
-		}
+		printf("\nNO INPUT\n");
+		return 1;
 	}
-	//^^^ works currently. finds all x's and returns the indexes of them into the array targetVarPos
-	//make a copy of the function.
+	if (cr == TOO_LONG)
+	{
+		printf("Input too long [%s]\n", deriv);
+		return 1;
+	}
+
+	//make a copy of the function to working function
 	for (int i = 0; i < strlen(function); i++)
 	{
 		workingFunction[i] = function[i];
 	}
-
-	for (int i = 0; i < strlen(workingFunction); i++)
-	{
-		workingFunction[targetVarPos[i]] = seedValue;
-	}
-	//combine this and the function that finds the x's ez mode
+	double result1 = compute(workingFunction, deriv, seedValue, 1);
+	printf("How many times do you want to run the iteration?");
+	int r = 0;
+	scanf_s("%i", r);
+	double result2 = compute(workingFunction, deriv, result1, r);
 	//enter into api, return deriv into new array called deriv
-
 	//first compute iteration here, everyone following is done using the function.
-	double r = te_interp(workingFunction, 0);
-	printf("%f", r);
 	//need to figure out how to use a web api without nuget shit.... dis iz prablem...
 	getchar();
 
 }
 
-int compute(char function[128], char deriv[128])
+double compute(char function[128], char deriv[128], double value, int repeat)
 {
+	double done = 0;
+	while (repeat >= 0)
+	{
+		double x;
+		te_variable vars[] = { { "x", &x } };
+		int err1;
+		te_expr *func = te_compile(function, vars, 2, &err1);
+		x = value;
+		double h1 = te_eval(func); /* Returns 5. */
+		te_free(func);
 
+		int err2;
+		te_expr *deriv = te_compile(deriv, vars, 2, &err2);
+		x = value;
+		double h2 = te_eval(deriv); /* Returns 5. */
+		te_free(deriv);
+
+		double s1 = h1 / h2;
+
+		double done = done - s1;
+		printf("%d", done);
+	}
+	return done;
 }
 
 
